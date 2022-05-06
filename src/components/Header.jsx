0c2d98b6ac56abc9c906c8ab3f5c319c6d5c51e1
maksim makovkin <maksim.makovkin@neuro-city.ru>
2022-05-06 08:55:35 +0300

    My first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://raddevon.com/wp-content/uploads/2018/10/react.jpg" alt=""/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div><img
+                src="https://planetofhotels.com/guide/sites/default/files/styles/node__blog_post__bp_banner/public/2020-06/Exotic-tropical-beach-sunse.jpg"  alt=""/>
+            </div>
+            <div>avatar + description</div>
+            <div>
+                My Posts
+                <div>New Post</div>
+            </div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    )
+}
+
+export default Profile
